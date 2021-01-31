08e3077d37f810ab18197aae354729a8a3a34d57
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-31 11:41:26 -0500

    DO NOT MERGE THIS BRANCH WITH MASTER EVER, IT WILL BREAK DEPLOTMENT

--- database/models/index.js
+++ database/models/index.js
@@ -9,6 +9,12 @@ Character.belongsTo(User)
 // allows for changes in database
 // FORCE TRUE DELETES ALL THE DATA IN THE DATABASE, ONLY USE IF MODIFYING MODELS
 // db.sequelize.sync({ force: true })
+
+//****************************************
+// THIS BRANCH IS FOR LOCAL TESTING ONLY! DO NOT MERGE THIS WITH MASTER
+// JUST CHECKOUT BACK TO MASTER AND ADD CHANGES THERE IF NEEDED
+//*****************************************
+
 db.sequelize.sync({ alter: true })
 
 module.exports = {
